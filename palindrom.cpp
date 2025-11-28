f4f4f72de3ec106b15482ce0865eaca13ebb09c5
5691snehitha <5691snehitha@gmail.com>
2025-11-28 20:11:27 +0530

    Update palindrom.cpp

--- palindrom.cpp
+++ palindrom.cpp
@@ -1,21 +1,21 @@
-#include<stdio.h>
-#include<math.h>
-int main (void)
+#include<stdio.h>
+int main ()
 {
 	int num, rem, rev= 0, temp;
 	printf("enter any number :");
 	scanf("%d",&num);
 	temp=num;
 	
-	while(num>0)
+	while(num!=0)
 	{
 		rem = num%10;
 		rev = (rev*10)+rem;
 		num = num/10;
 		
 	}
-	if (rev == num)
-	printf("%d is a palindrome",temp);
+	if (temp == rev)
+	printf("palindrome number");
 	else
-	printf("%d is not a palindrome",temp);
+	printf("not a palindrome number");
+return 0;
 }
