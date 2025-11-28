20aa59ba9a3f1eb5a65168fe7b45839d569e902b
5691snehitha <5691snehitha@gmail.com>
2025-11-28 21:48:09 +0530

    Update min&max.cpp

--- min&max.cpp
+++ min&max.cpp
@@ -1,19 +1,27 @@
 #include<stdio.h>
-int main(void)
+int main()
 {
-	int arr[6], i, min, max;
-	//reading values into the arr[6]
-	printf("enter any 6-elements for arr[6]:\n");
-	for(i=0;i<6;i++)
-	    scanf("%d",&arr[0]);
-		//finding minimum and maximum
-		min=max=arr[0];
-		for(i=0;i<6;i++)
-		{
-			if(min>arr[i]) min=arr[i];
-			if(max<arr[i]) max=arr[i];
-		}
-		printf("\nminimum in arr[6]: %d", min);
-		printf("\nmaximum in arr[6]: %d", max);
-		
+	intarr[10],maxmin,size,i,sum=0;
+float avg;
+printf("enter size of the array (<i0););
+scanf("%d",&size);
+printf ("enter elements of the array:");
+for(i=0;i<size;i++)
+{
+scanf("%d",&arr[i]);
+sum+=arr[i];
+}
+min=max=arr[0];
+for(i=1;i<size;i++)
+{
+if(arr[i]>max)
+max=arr[i];
+if(arr[i]<min)
+min=arr[i];
+}
+avg=(float) sum/size;
+printf ("\n maximum is:%d",max);
+printf ("\n minimum is:%d",min);
+printf ("\n average is :%2.f",avg);
+return 0b
 }
