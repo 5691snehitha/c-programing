89b1d8a25d532ec138ea98159dd05b86c0180ba7
5691snehitha <5691snehitha@gmail.com>
2025-11-29 07:37:07 +0530

    Update multiplication table

--- avg.cpp
+++ avg.cpp
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main(void)
 {
-	int arr[6], i, sum=0;
-	float avg;
-	printf("enter value for arr[6]:/n");
-	for(i=0;i<6;i++)
-	scanf("%d", &arr[i]);
-	for (i=0;i<6;i++)
-	sum=sum+arr[i];
-	avg=(float)sum/6;
-	printf("\nsum of the elements in an[6] is : %d",sum);
-	printf("\naverage of elements in an [6] is:%lf",avg);
+	int num,rows,i;
+printf ("enter the number:");
+scanf ("%d",&num:);
+printf ("\n multiplication table for %d\n",num*:);
+for(i=1;i<=rows;i++);
+{
+printf ("%d*%d=%d\n",num;num*:);
+}
+return 0;
 }
